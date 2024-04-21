082df2bdafaa8dc29d0515188407ef9bfbe9361d
Hiroki Yamazaki <yhirokiy@amazon.co.jp>
2024-04-21 22:24:22 +0900

    update projen.ts

--- .projenrc.ts
+++ .projenrc.ts
@@ -4,7 +4,7 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   defaultReleaseBranch: 'main',
   name: 'cdk-illuminations-summit2024',
   projenrcTs: true,
-
+  deps: ['deploy-time-build'],
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
